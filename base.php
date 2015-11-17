3138c0009180c95182b669f7724136d2c8c9adba
jeffsee55 <jeffsee.55@gmail.com>
2015-11-17 18:27:26 -0500

    Added different logo

--- base.php
+++ base.php
@@ -15,11 +15,28 @@ use Roots\Sage\Wrapper;
 			<header class="ip-header">
 				<h1 class="ip-logo">
 					<svg class="ip-inner" width="100%" height="100%" viewBox="0 0 300 160" preserveAspectRatio="xMidYMin meet" aria-labelledby="logo_title">
-            <title  id="logo_title">Delightful Demonstrations by Codrops</title>
-            <polygon opacity="0.4" fill="#29ABE2" stroke="#29ABE2" stroke-miterlimit="10" points="79.4,107.9 228.8,5.6 223.4,95.7 "/>
-            <polygon opacity="0.4" fill="#29ABE2" stroke="#29ABE2" stroke-miterlimit="10" points="50.3,82.6 213.7,27.8 196.1,107.9 "/>
-            <polygon opacity="0.4" fill="#29ABE2" stroke="#29ABE2" stroke-miterlimit="10" points="26,55.9 164.4,116.1 191.2,53.9 "/>
-            <text transform="matrix(1 0 0 1 13.3985 152.1455)" opacity="0.39" fill="#29ABE2" stroke="#29ABE2" stroke-miterlimit="10" font-family="'Lato-Light'" font-size="31.9326" letter-spacing="9">NTBCAPITAL</text>
+            <rect x="39.7" y="7.5" fill="#0B1033" stroke="#DBB963" stroke-width="3" stroke-miterlimit="10" width="210.8" height="216.4"/>
+            <polygon fill="#0B1033" stroke="#DBB963" stroke-width="2" stroke-miterlimit="10" points="56.8,228 196.4,134.5 250.3,228 "/>
+            <polygon fill="#0B1033" stroke="#DBB963" stroke-width="2" stroke-miterlimit="10" points="55.6,198.3 226.3,149.1 250.3,228 
+              41.7,228 "/>
+            <polygon fill="#0B1033" stroke="#DBB963" stroke-width="2" stroke-miterlimit="10" points="250.5,230 250.3,175.9 40,198.5 
+              39.7,230 "/>
+            <g>
+              <path fill="#DBB963" d="M113.2,38.8V90c-0.3-0.4-1-1.1-2.1-2.2s-2.4-2.4-4-3.9c-1.6-1.5-3.3-3.2-5.2-5c-1.9-1.8-3.8-3.6-5.8-5.5
+                c-2-1.9-3.9-3.7-5.8-5.5c-1.9-1.8-3.6-3.4-5.1-4.9c-1.5-1.5-2.8-2.8-3.9-3.8c-1.1-1.1-1.7-1.8-2-2.2c0-0.1-0.2-0.3-0.5-0.7
+                c-0.3-0.4-0.6-0.7-0.8-1l0.2,2.1v30.3H70V36.6c0.3,0.4,1,1.1,2.1,2.2c1.1,1.1,2.4,2.3,4,3.8c1.6,1.5,3.3,3.1,5.2,4.8
+                c1.9,1.7,3.8,3.5,5.8,5.3c2,1.8,3.9,3.6,5.8,5.4c1.9,1.8,3.6,3.4,5.2,4.8c1.6,1.5,2.9,2.7,3.9,3.7c1.1,1,1.7,1.7,2,2.1l1.3,1.9
+                l-0.2-2.3V38.8H113.2z"/>
+              <path fill="#DBB963" d="M167.3,38.8v7.9h-12.1v41.1h-8.3V46.6h-11.8v-7.9H167.3z"/>
+              <path fill="#DBB963" d="M189.2,87.7V38.8h13.4c2.4,0,4.6,0.2,6.6,0.7c2,0.5,3.6,1.2,5,2.2c1.4,1,2.5,2.3,3.2,4
+                c0.7,1.6,1.1,3.6,1.1,5.9c0,1.3-0.1,2.5-0.4,3.7c-0.2,1.2-0.6,2.3-1.2,3.3c-0.6,1-1.3,1.9-2.2,2.7c-0.9,0.8-2,1.3-3.4,1.7
+                c1.1,0.2,2.2,0.6,3.4,1.1s2.2,1.3,3.1,2.2c0.9,0.9,1.6,2.1,2.2,3.4s0.8,3,0.8,4.8c0,2.4-0.5,4.4-1.4,6.1c-0.9,1.7-2.1,3.1-3.6,4.1
+                c-1.5,1.1-3.2,1.9-5.2,2.3c-2,0.5-4,0.7-6.1,0.7H189.2z M197.4,46.2v13.3h6.2c2.3,0,4-0.7,5.1-2c1.1-1.3,1.6-3,1.6-4.8
+                c0-2.2-0.7-3.8-2-4.9c-1.3-1.1-3.2-1.6-5.4-1.6H197.4z M197.4,66.5v13.8h6.4c1.2,0,2.3-0.1,3.4-0.4c1.1-0.3,2-0.7,2.8-1.3
+                c0.8-0.6,1.4-1.3,1.9-2.1c0.4-0.9,0.7-1.9,0.7-3.1c0-1.3-0.3-2.4-0.8-3.3c-0.5-0.9-1.3-1.6-2.1-2.1c-0.9-0.5-1.9-0.9-3.1-1.1
+                c-1.1-0.2-2.3-0.3-3.5-0.3H197.4z"/>
+            </g>
+            <line fill="none" stroke="#DBB963" stroke-width="2" stroke-miterlimit="10" x1="67" y1="103" x2="222" y2="103"/>
           </svg>
 				</h1>
 				<div class="ip-loader">
